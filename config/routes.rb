8758c9f819f986e08608ee70be0898f1c4226a72
windo <herwindo.artono@gmail.com>
2025-05-06 22:30:18 +0700

    feat(home): update home page feat(blog): cleanup blog post list

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # temporary
+  get '/works', to: "home#index", as: :works
+  get '/case-studies', to: "home#index", as: :case_studies
+  
   # Defines the root path route ("/")
   root "home#index"
 end
